d971cd237ba677c06f715e2f31e8d0b586ad2b5d
valeeff <valentina.vf.ferretti@gmail.com>
2026-02-11 23:36:01 +0100

    deleted useless files

--- msg_editor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Replaces commit message if it matches the target
-grep -q "stuff to publish extension" "$1" && echo "Chore: Add release assets and privacy policy" > "$1" || true
--- sequence_editor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Replaces 'pick b605156' with 'reword b605156'
-sed -i.bak 's/^pick b605156/reword b605156/' "$1"
